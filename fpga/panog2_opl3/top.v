c6805b09c6e63095166af688d1a265c293830a6a
Skip Hansen <skip@gfrn.org>
2019-12-08 12:52:20 -0800

    Basic SOC hello world fpga working.

--- fpga/panog2_opl3/top.v
+++ fpga/panog2_opl3/top.v
@@ -4,11 +4,8 @@
 module top
 (
      input           SYSCLK
-    ,inout           pano_button
     ,output          GMII_RST_N
-    ,output          led_red
-    ,inout           led_green
-    ,inout           led_blue
+    ,output          led_blue
 
     // UART
     ,input           uart_txd_i
@@ -20,22 +17,16 @@ module top
     ,output          flash_si_o
     ,input           flash_so_i
 
-     // WM8750 Codec
-     ,output codec_mclk
-     ,output codec_bclk
-     ,output codec_dacdata
-     ,output codec_daclrck
-     ,input  codec_adcdata
-     ,output codec_adclrck
-     ,inout  codec_scl
-     ,inout  codec_sda
+    // GPIO Headers
+    ,inout  [11:0]   porta
+    ,inout  [11:0]   portb
 );
 
-// Generate 32 Mhz system clock and 25 Mhz audio clock from 125 Mhz input clock
-wire clk32;
-wire clk25;
+wire [11:0]   porta;
+wire [11:0]   portb;
 
-assign codec_mclk = clk25;
+// Generate 32 Mhz system clock from 125 Mhz input clock
+wire clk32_i;
 
 IBUFG clkin1_buf
 (   .O (clkin1),
@@ -44,25 +35,22 @@ IBUFG clkin1_buf
 
 PLL_BASE
     #(.BANDWIDTH              ("OPTIMIZED"),
-      .CLKFBOUT_MULT          (16),
+      .CLKFBOUT_MULT          (32),
       .CLKFBOUT_PHASE         (0.000),
       .CLK_FEEDBACK           ("CLKFBOUT"),
-      .CLKIN_PERIOD           (6.666),
+      .CLKIN_PERIOD           (8.000),
       .COMPENSATION           ("SYSTEM_SYNCHRONOUS"),
-      .DIVCLK_DIVIDE          (3),
+      .DIVCLK_DIVIDE          (5),
       .REF_JITTER             (0.010),
       .CLKOUT0_DIVIDE         (25),
-      .CLKOUT0_DUTY_CYCLE     (0.500),
       .CLKOUT0_PHASE          (0.000),
-      .CLKOUT1_DIVIDE         (32),
-      .CLKOUT1_DUTY_CYCLE     (0.500),
-      .CLKOUT1_PHASE          (0.000),
+      .CLKOUT0_DUTY_CYCLE     (0.500)
     )
     pll_base_inst
       // Output clocks
      (.CLKFBOUT              (clkfbout),
-      .CLKOUT0               (clkout32),
-      .CLKOUT1               (clkout25),
+      .CLKOUT0               (clkout0),
+      .CLKOUT1               (clkout1_unused),
       .CLKOUT2               (clkout2_unused),
       .CLKOUT3               (clkout3_unused),
       .CLKOUT4               (clkout4_unused),
@@ -82,12 +70,8 @@ BUFG clkf_buf
   .I (clkfbout));
 
 BUFG clk32_buf
-  (.O (clk32),
-   .I (clkout32));
-
-BUFG clk25_buf
-(.O (clk25),
- .I (clkout325));
+  (.O (clk32_i),
+   .I (clkout0));
 
 //-----------------------------------------------------------------
 // Reset
@@ -97,7 +81,7 @@ wire rst;
 reset_gen
 u_rst
 (
-    .clk_i(clk32),
+    .clk_i(clk32_i),
     .rst_o(rst)
 );
 
@@ -122,11 +106,15 @@ fpga_top
    ,.BAUDRATE(1000000)   // SoC UART baud rate
    ,.UART_SPEED(1000000) // Debug bridge UART baud (should match BAUDRATE)
    ,.C_SCK_RATIO(50)     // SPI clock divider (SPI_CLK=CLK_FREQ/C_SCK_RATIO)
+`ifdef CPU_SELECT_ARMV6M
+   ,.CPU("armv6m")       // riscv or armv6m
+`else
    ,.CPU("riscv")        // riscv or armv6m
+`endif
 )
 u_top
 (
-     .clk_i(clk32)
+     .clk_i(clk32_i)
     ,.rst_i(rst)
 
     ,.dbg_rxd_o(dbg_txd_w)
@@ -154,41 +142,25 @@ assign flash_cs_o  = spi_cs_w[0];
 assign spi_so_w    = flash_so_i;
 
 //-----------------------------------------------------------------
-// GPIO bits
-// 0: reserved for CPU reset (?)
-// 1: Pano button
-// 2: Output only - red LED
-// 3: In/out - green LED
-// 4: In/out - blue LED
-// 5: Wolfson codec SDA
-// 6: Wolfson codec SCL
-// 9...31: Not implmented
+// GPIO (Port A=gpio[11:0],...,Port B=gpio[27:16])
 //-----------------------------------------------------------------
-
-assign pano_button = gpio_out_en_w[1]  ? gpio_out_w[1]  : 1'bz;
-assign gpio_in_w[1]  = pano_button;
-
-assign led_red = gpio_out_w[2];
-assign gpio_in_w[2]  = led_red;
-
-assign led_green = gpio_out_en_w[3]  ? gpio_out_w[3]  : 1'bz;
-assign gpio_in_w[3]  = led_green;
-
-assign led_blue = gpio_out_en_w[4]  ? gpio_out_w[4]  : 1'bz;
-assign gpio_in_w[4]  = led_blue;
-
-assign codec_sda = gpio_out_en_w[5]  ? gpio_out_w[5]  : 1'bz;
-assign gpio_in_w[5]  = codec_sda;
-
-assign codec_scl = gpio_out_en_w[6]  ? gpio_out_w[6]  : 1'bz;
-assign gpio_in_w[6]  = codec_sda;
-
-
 genvar i;
 generate
-for (i=5; i < 31; i=i+1)
+for (i=0; i < 12; i=i+1)
 begin
-    assign gpio_in_w[i]  = 1'b0;
+    assign porta[i]        = gpio_out_en_w[0+i]  ? gpio_out_w[0+i]  : 1'bz;
+    assign portb[i]        = gpio_out_en_w[16+i] ? gpio_out_w[16+i] : 1'bz;
+
+    assign gpio_in_w[0+i]  = porta[i];
+    assign gpio_in_w[16+i] = portb[i];
+end
+endgenerate
+
+generate
+for (i=12; i < 16; i=i+1)
+begin
+    assign gpio_in_w[0+i]  = 1'b0;
+    assign gpio_in_w[16+i] = 1'b0;
 end
 endgenerate
 
@@ -199,7 +171,7 @@ endgenerate
 //synthesis attribute IOB of uart_rxd_o is "TRUE"
 reg txd_q;
 
-always @ (posedge clk32 or posedge rst)
+always @ (posedge clk32_i or posedge rst)
 if (rst)
     txd_q <= 1'b1;
 else
@@ -208,51 +180,16 @@ else
 // 'OR' two UARTs together
 assign uart_rxd_o  = txd_q;
 
-// Audio
+assign led_blue = uart_rxd_o;
 
-wire signed [15:0] opl2_channel_a;
-wire signed [15:0] opl2_channel_b;
-wire signed [15:0] opl2_channel_c;
-wire signed [15:0] opl2_channel_d;
-wire opl3_sample_clk;
-wire codec_bclk;
-
-opl3 opl3_inst (
-  .clk(clk33),  // system clock
-  .clk_opl3(clk25),  // 25 MHz OPL2 clock
-  .opl3_we(),   // register write
-  .opl3_data(), // register data
-  .opl3_adr(),  // register address
-  .channel_a(opl2_channel_a), // output channel a
-  .channel_b(opl2_channel_b), // output channel b
-  .channel_c(opl2_channel_c),
-  .channel_d(opl2_channel_d),
-  .sample_clk(audio_sample_clk),
-  .sample_clk_128(codec_bclk)   // 128 X sample rate clock
-);
-
-wire signed [18:0] left_pre, right_pre;
-
-assign left_pre = opl2_channel_a + opl2_channel_c;
-assign right_pre = opl2_channel_b + opl2_channel_d;
-
-wire signed [15:0] left, right;
-
-assign left = left_pre > 32767 ? 16'hffff : left_pre < -32768 ? 16'h0000 : left_pre[15:0];
-assign right = right_pre > 32767 ? 16'hffff : right_pre < -32768 ? 16'h0000: right_pre[15:0];
-
-audio audio_out (
-    .clk25(clk25),
-    .reset25(),
-    .codec_bclk(codec_bclk),
-    .audio_dacdat(codec_dacdata),
-    .audio_daclrc(codec_daclrck),
-    .audio_adcdat(codec_adcdata),
-    .audio_adclrc(codec_adclrck),
-    .audio_right_sample(right),
-    .audio_left_sample(left),
-    .audio_sample_clk(audio_sample_clk)
-);
+// ODDR2 debug_buf (
+//     .D0(1'b1),
+//     .D1(1'b0),
+//     .C0(uart_txd_i),
+//     .C1(!uart_txd_i),
+//     .CE(1'b1),
+//     .Q(led_blue)
+// );
 
 //-----------------------------------------------------------------
 // Tie-offs
